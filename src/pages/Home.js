9d6caf3e55b74df2ec68c016bfa4f8145f11e2d6
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-03-15 02:12:58 +0000

    took out old headshot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import myImage from "../images/oldheadshot.jpg";
 
 function Home() {
   return (
@@ -15,7 +14,6 @@ function Home() {
       }}
     >
       <Card sx={{ width: 400, marginBottom: "100px" }}>
-        <CardMedia sx={{ height: 400 }} image={myImage} title="Henry Green" />
         <CardContent sx={{ textAlign: "center" }}>
           <Typography gutterBottom variant="h5" component="div">
             Henry James Green
